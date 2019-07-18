e5e55075b71c1c7dbb70a4a646debd07c018ea7a
josefaidt <josef.aidt@gmail.com>
2019-07-18 14:51:14 -0500

    set defaults, better url construction

--- theme/src/hooks/useWeather.js
+++ theme/src/hooks/useWeather.js
@@ -4,21 +4,16 @@ const defaultLocation = '37.8267,-122.4233'
 const proxy = 'https://cors-anywhere.herokuapp.com'
 const forecastUrl = key => `https://api.darksky.net/forecast/${key}`
 
-const useDarkSkyWeather = (location, key) => {
+const useDarkSkyWeather = (location = defaultLocation, key = process.env.API_KEY) => {
   const [response, setResponse] = React.useState({})
   const [error, setError] = React.useState(null)
   const [pending, setPending] = React.useState(true)
   React.useEffect(() => {
     setPending(true)
     let reqUrl = ''
-    const url = `${proxy}/${forecastUrl(key)}`
-    if (!location || location === null) {
-      reqUrl = `${url}/${defaultLocation}`
-    } else {
-      const longitude = location.longitude.toFixed(4)
-      const latitude = location.latitude.toFixed(4)
-      reqUrl = `${url}/${latitude},${longitude}`
-    }
+    const url = `${proxy}/https://api.darksky.net/forecast/${key}`
+    const { longitude, latitude } = location
+    reqUrl = `${url}/${latitude},${longitude}`
     fetch(reqUrl)
       .then(r => r.json())
       .then(data => {
