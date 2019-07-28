4037283c2c6d91b1d88312f78f5e6c86e68a5aa7
josef <josef.aidt@gmail.com>
2019-07-28 16:11:58 -0500

    fetch error handling

--- theme/src/components/Skeleton.js
+++ theme/src/components/Skeleton.js
@@ -11,39 +11,45 @@ const shortcodes = {
 }
 
 console.log('PROCESS ENV', process.env)
+const key = process.env.API_KEY
 
 const Skeleton = ({ children, pageContext }) => {
-  const { data: geo, pending, error } = useGeoState()
+  const { data: geo, pending, geoError } = useGeoState()
   const [weatherData, setWeatherData] = React.useState(null)
+  const [error, setError] = React.useState(null)
   React.useEffect(() => {
     async function fetchData() {
-      const key = process.env.API_KEY
       const proxy = 'https://cors-anywhere.herokuapp.com'
       const url = `${proxy}/https://api.darksky.net/forecast/${key}`
       const { longitude, latitude } = geo
       const reqUrl = `${url}/${latitude},${longitude}`
       const response = await fetch(reqUrl)
       const data = await response.json()
-      console.log('FETCHED DATA', data)
-      await setWeatherData(data)
+      if (data.error) {
+        await setError(data)
+      } else {
+        await setWeatherData(data)
+      }
     }
-    console.log(weatherData)
+    console.log('WEATHER DATA STATE', weatherData)
     const s = localStorage
     const cache = s.getItem('weather')
-    if (!pending && weatherData === null && cache === null) {
-      fetchData()
-    } else if (!pending && weatherData !== null) {
-      if (cache && cache !== weatherData) {
-        console.log('MADE IT HERE')
-        localStorage.removeItem('weather')
-        localStorage.setItem('weather', JSON.stringify(weatherData))
-      } else {
-        console.log('MADE IT ELSE')
-        localStorage.setItem('weather', JSON.stringify(weatherData))
+    if (!error) {
+      if (!pending && weatherData === null && cache === null) {
+        fetchData()
+      } else if (!pending && weatherData !== null) {
+        if (cache && cache !== weatherData) {
+          console.log('MADE IT HERE')
+          localStorage.removeItem('weather')
+          localStorage.setItem('weather', JSON.stringify(weatherData))
+        } else {
+          console.log('MADE IT ELSE')
+          localStorage.setItem('weather', JSON.stringify(weatherData))
+        }
+      } else if (cache !== null && weatherData === null) {
+        console.log('CACHE NOT NULL, SETTING WEATHERDATA')
+        setWeatherData(JSON.parse(cache))
       }
-    } else if (cache !== null && weatherData === null) {
-      console.log('CACHE NOT NULL, SETTING WEATHERDATA')
-      setWeatherData(JSON.parse(cache))
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [geo, pending])
@@ -74,7 +80,11 @@ const Skeleton = ({ children, pageContext }) => {
         <Container>
           <MDXProvider components={shortcodes}>{children}</MDXProvider>
           <br />
-          <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+          {error ? (
+            <pre>{JSON.stringify(error, null, 2)}</pre>
+          ) : (
+            <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+          )}
           {/* {pending ? <span>Pending...</span> : null} */}
           {/* {geoLocation.error ? (
             <span>{geoLocation.error.message}</span>
