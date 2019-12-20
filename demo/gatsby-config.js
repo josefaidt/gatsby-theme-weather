a9e36bcefbece2eab51d6161d1001156b515d878
josef <josef.aidt@gmail.com>
2019-12-20 16:32:04 -0600

    linting and prettier

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,16 +1,16 @@
-const d = require('dotenv')
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-  d.config({
-    path: `.env.development`,
-  })
-}
+// const d = require('dotenv')
+// if (process.env.NODE_ENV !== 'PRODUCTION') {
+//   d.config({
+//     path: `.env.development`,
+//   })
+// }
 
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-theme-weather',
       options: {
-        apiKey: process.env.API_KEY,
+        // apiKey: process.env.API_KEY,
       },
     },
     {
