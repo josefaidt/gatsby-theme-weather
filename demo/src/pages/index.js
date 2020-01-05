0792df00ed09a78c9c3b1860c672827ec831c1f1
josef <josef.aidt@gmail.com>
2020-01-05 14:17:08 -0600

    add CurrentWeather

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from 'gatsby-theme-weather/src/layouts/default'
+import { CurrentWeather } from 'gatsby-theme-weather/src/components/weather'
 import { useWeatherState } from 'gatsby-theme-weather/src/helpers/WeatherContext'
 
 const HomePage = props => {
   const weatherState = useWeatherState()
   return (
     <Layout>
+      <CurrentWeather />
       <pre>{JSON.stringify(weatherState, null, 2)}</pre>
     </Layout>
   )
--- theme/src/components/Card.js
+++ theme/src/components/Card.js
@@ -3,10 +3,65 @@ import styled from 'styled-components'
 import { useCurrentTheme } from '../helpers/ThemeContext'
 
 const StyledCard = styled.article`
-  background-color: ${({ theme }) => `${theme.colors.primary}d9` || 'whitesmoke'};
-  color: ${({ theme }) => theme.colors.accent || 'black'};
+  background-color: ${({ theme }) => theme.colors.background || 'white'};
+  position: relative;
+  overflow: hidden;
+  /* border: 1px solid red; */
 
-  padding: 1rem;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  max-height: 50vh;
+  overflow-y: scroll;
+
+  .gtw--card--header-title__container {
+    margin-top: 0.5rem;
+    margin-bottom: 0.4rem;
+  }
+  .gtw--card--description {
+    margin-bottom: 0.2rem;
+    font-size: 90%;
+  }
+
+  button#gtw--card--button__close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+
+    width: 1.5rem;
+    height: 1.3rem;
+    border: none;
+    border-bottom-left-radius: 0.125rem;
+
+    svg {
+      height: 0.5rem;
+      width: 0.5rem;
+      line {
+        stroke: ${({ theme }) => theme.colors.text || 'black'};
+      }
+    }
+
+    transition: all 0.3s ease;
+    &:hover {
+      transition: all 0.3s ease;
+      background-color: ${({ theme }) => `${theme.colors.text}b3` || '#bbbbbbcc'};
+      svg line {
+        stroke: ${({ theme }) => theme.colors.background || 'white'};
+      }
+    }
+  }
+}
+  /* background-color: ${({ theme }) => `${theme.colors.primary}d9` || 'whitesmoke'};
+  color: ${({ theme }) => theme.colors.accent || 'black'}; */
+
+  /* padding: 1rem;
   margin: 1rem 0;
   border-radius: 0.5rem;
   font-weight: bold;
@@ -27,14 +82,14 @@ const StyledCard = styled.article`
       justify-content: center;
       align-items: center;
     }
-  }
+  } */
 
   svg {
     fill: ${({ theme }) => theme.colors.accent || 'black'};
     transform: scale(1.5);
   }
 
-  @media only screen and (max-width: 768px) {
+  /* @media only screen and (max-width: 768px) {
     header {
       grid-template-columns: 1fr;
       grid-template-rows: 5rem auto;
@@ -43,7 +98,7 @@ const StyledCard = styled.article`
         grid-row: 2;
       }
     }
-  }
+  } */
 `
 
 const Card = props => {
