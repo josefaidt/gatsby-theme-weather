16d9830fd32a2127e9f74e0412ac46b9c7b2892d
josef <josef.aidt@gmail.com>
2020-01-05 15:10:53 -0600

    add service worker confirm

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -4,6 +4,17 @@ import { GeoContextProvider } from './src/helpers/GeoContext'
 import { WeatherProvider } from './src/helpers/WeatherContext'
 import { NotificationProvider } from './src/helpers/NotificationContext'
 
+export const onServiceWorkerUpdateReady = () => {
+  // eslint-disable-next-line no-alert
+  const answer = window.confirm(
+    `This application has been updated. Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
+
 // eslint-disable-next-line react/display-name
 export const wrapRootElement = ({ element }) => {
   return (
